c312dd05c88f07c3f9b8661e4be2565d778608c3
Kiithsan <lostkiith@countermail.com>
2019-12-10 14:00:45 +0000

    continued moving manager javascript functions to a new file.

--- src/main/resources/static/JS/home.js
+++ src/main/resources/static/JS/home.js
@@ -565,73 +565,7 @@ function behavioralSignsMissedAppAll() {
 
 //MANAGER
 //APPOINTMENTS
-//function relating to creating a Appointment
 
-
-function getAppropriateStaff() {
-    let selectClient = document.getElementById("clientForAppointment");
-    let opt = selectClient.options[selectClient.selectedIndex];
-    let clientID = JSON.parse(opt.value);
-
-    let select = document.getElementById("staffMembers");
-    let option;
-    let i;
-    for (i = select.options.length - 1; i >= 0; i--) {
-        select.remove(i);
-    }
-
-    $.get("http://localhost:8080/Charity/getAppropriateStaff/",
-        {
-            clientNationalHealthServiceNumber: clientID.nationalHealthServiceNumber
-        },
-        function (data) {
-            for (let i = 0; i < data.length; i++) {
-                option = document.createElement('option');
-                option.value = JSON.stringify(data[i]);
-                option.text = data[i].firstName + "  " + data[i].lastName;
-                select.add(option);
-            }
-            if (data.length === 0) {
-                option = document.createElement('option');
-                option.value = JSON.stringify(data[i]);
-                option.text = "no Staff available.";
-                select.add(option);
-            }
-            select.hidden = false;
-        }
-    ).fail(function () {
-    });
-
-}
-
-function submitCreateAppointment() {
-
-    let select = document.getElementById("clientForAppointment");
-    let opt = select.options[select.selectedIndex];
-    let clientID = JSON.parse(opt.value);
-
-    select = document.getElementById("staffMembers");
-    opt = select.options[select.selectedIndex];
-    let staffMembersID = JSON.parse(opt.value);
-
-    select = document.getElementById("day");
-    let day = select.options[select.selectedIndex].text;
-
-    $.post(
-        "http://localhost:8080/Charity/createAnAppointment/",
-        {
-            day: day,
-            startTime: document.getElementById("startTime").value,
-            endTime: document.getElementById("endTime").value,
-            NHSnumber: clientID.nationalHealthServiceNumber,
-            ninNumber: staffMembersID.ninNumber,
-        },
-        function (data) {
-            document.getElementById("informAppointment").innerText = "Appointment successfully created";
-        }).fail(function (data) {
-        document.getElementById("informAppointment").innerText = data.responseJSON.message;
-    });
-}
 
 //removing a staff members appointment.
 function openRemoveStaffApp() {
--- src/main/resources/static/JS/manager.js
+++ src/main/resources/static/JS/manager.js
@@ -17,5 +17,69 @@ function openCreateAppointmentForm() {
 
     //populate list of clients.
     populateClientListManager("clientForAppointment");
+}
 
+function getAppropriateStaff() {
+    let selectClient = document.getElementById("clientForAppointment");
+    let opt = selectClient.options[selectClient.selectedIndex];
+    let clientID = JSON.parse(opt.value);
+
+    let select = document.getElementById("staffMembers");
+    let option;
+    let i;
+    for (i = select.options.length - 1; i >= 0; i--) {
+        select.remove(i);
+    }
+
+    $.get("http://localhost:8080/Charity/getAppropriateStaff/",
+        {
+            clientNationalHealthServiceNumber: clientID.nationalHealthServiceNumber
+        },
+        function (data) {
+            for (let i = 0; i < data.length; i++) {
+                option = document.createElement('option');
+                option.value = JSON.stringify(data[i]);
+                option.text = data[i].firstName + "  " + data[i].lastName;
+                select.add(option);
+            }
+            if (data.length === 0) {
+                option = document.createElement('option');
+                option.value = JSON.stringify(data[i]);
+                option.text = "no Staff available.";
+                select.add(option);
+            }
+            select.hidden = false;
+        }
+    ).fail(function () {
+    });
+
+}
+
+function submitCreateAppointment() {
+
+    let select = document.getElementById("clientForAppointment");
+    let opt = select.options[select.selectedIndex];
+    let clientID = JSON.parse(opt.value);
+
+    select = document.getElementById("staffMembers");
+    opt = select.options[select.selectedIndex];
+    let staffMembersID = JSON.parse(opt.value);
+
+    select = document.getElementById("day");
+    let day = select.options[select.selectedIndex].text;
+
+    $.post(
+        "http://localhost:8080/Charity/createAnAppointment/",
+        {
+            day: day,
+            startTime: document.getElementById("startTime").value,
+            endTime: document.getElementById("endTime").value,
+            NHSnumber: clientID.nationalHealthServiceNumber,
+            ninNumber: staffMembersID.ninNumber,
+        },
+        function (data) {
+            document.getElementById("informAppointment").innerText = "Appointment successfully created";
+        }).fail(function (data) {
+        document.getElementById("informAppointment").innerText = data.responseJSON.message;
+    });
 }
